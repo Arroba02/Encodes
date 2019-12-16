5525e3bfb3a5e2c8c18589e1111d77ec0c0715ca
Arroba02 <58848390+Arroba02@users.noreply.github.com>
2019-12-16 13:53:35 +0100

    primero

---
+++ js/js1.js
@@ -0,0 +1,7 @@
+
+//archivo javascript1
+
+/*var alto_ventana = window.innerHeight
+var ancho_ventana = window.innerWidth
+document.write(ancho_ventana +" x "+ alto_ventana)
+document.getElementById("encabezado").style.height = String(alto_ventana*0.10)+"px"*/
